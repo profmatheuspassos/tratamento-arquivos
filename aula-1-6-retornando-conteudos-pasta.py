be4b92acaaa28d081c44044144ce8c4718da40e2
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-03-28 07:51:40 +0200

    Atualização de nomes de arquivos e inclusão de novas aulas

---
+++ aula-1-6-retornando-conteudos-pasta.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import os
+
+os.system("clear")
+
+# Listando arquivos de uma pasta
+print("os.listdir(Path.cwd())")
+print(os.listdir(Path.cwd()))
+print("\nPath.cwd().glob(\"*\")")
+print(Path.cwd().glob("*"))
+print("\nlist(Path.cwd().glob(\"*\"))")
+print(list(Path.cwd().glob("*")))
+
+# Listando arquivos de uma determinada extensão
+print("\nlist(Path.cwd().glob(\"*.py\"))")
+print(list(Path.cwd().glob("*.py")))
+
+# Listar tudo dentro de uma pasta
+print("\nlist(Path.cwd().glob(\"**/*\"))")
+print(list(Path.cwd().glob("**/*")))
+print("\nlist(Path.cwd().glob(\"**/*.txt\"))")
+print(list(Path.cwd().glob("**/*.txt")))
+
+# Validando caminhos
+print("\nNão existe")
+naoExiste = Path.home() / "naoExiste"
+print(naoExiste.exists())
+
+# Verificando se é arquivo ou pasta
+print("\nVerificando se é arquivo ou pasta")
+print(Path(__file__))
+print(Path(__file__).is_file())
+print(Path(__file__).parent)
+print(Path(__file__).parent.is_file())
+print(Path(__file__).parent)
+print(Path(__file__).parent.is_dir())
+print(Path(__file__).name)
+print(Path(__file__).stem)
+print(Path(__file__).suffix)
